7f418f3b9eb701367876162c1f439258733b9b95
shinta0806 <shinta.0806@gmail.com>
2024-11-28 00:14:36 +0900

    RelativePanel は背景を指定するとドロップを受け付ける。

--- TestRelativePanelDrop/ViewModels/MainViewModel.cs
+++ TestRelativePanelDrop/ViewModels/MainViewModel.cs
@@ -1,10 +1,58 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Xaml;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage;
 
 namespace TestRelativePanelDrop.ViewModels;
 
 public partial class MainViewModel : ObservableRecipient
 {
-    public MainViewModel()
-    {
-    }
+	public MainViewModel()
+	{
+	}
+
+	private String? _path;
+	public String? Path
+	{
+		get => _path;
+		set => SetProperty(ref _path, value);
+	}
+
+	public void DragOver(Object _, DragEventArgs args)
+	{
+		try
+		{
+			if (!args.DataView.Contains(StandardDataFormats.StorageItems))
+			{
+				return;
+			}
+			args.Handled = true;
+			args.AcceptedOperation = DataPackageOperation.Copy;
+		}
+		catch (Exception)
+		{
+		}
+	}
+
+	public async void Drop(Object _, DragEventArgs args)
+	{
+		try
+		{
+			if (!args.DataView.Contains(StandardDataFormats.StorageItems))
+			{
+				return;
+			}
+
+			args.Handled = true;
+			IReadOnlyList<IStorageItem> storageItems = await args.DataView.GetStorageItemsAsync();
+			foreach (IStorageItem storageItem in storageItems)
+			{
+				Path = storageItem.Path;
+			}
+		}
+		catch (Exception)
+		{
+		}
+	}
+
 }
